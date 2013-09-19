4fa5f56ab7e39d0ce38a431ed98b242a5596ea09
Joshua Kemmerling <joshuakemmerling@Joshuas-iMac.local>
2013-09-18 22:12:02 -0500

    nick

---
+++ nick/about.php
@@ -0,0 +1,16 @@
+<?php include "inc/header.php"; ?>
+	<section class="container_12 clearfix">
+		<img src="img/about.jpg" class="grid_12 mb20" height="360">
+		<div class="grid_6">
+			<h3 class="mb20">Welcome</h3>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+			<p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+			<p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+		</div>
+		<div class="grid_6">
+			<h3 class="mb20">Mission</h3>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+			<p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+		</div>
+	</section>
+<?php include "inc/footer.php"; ?>
---
+++ nick/inc/header.php
@@ -0,0 +1,37 @@
+<!-- https://code.google.com/p/tumblr-related-posts/ -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apokalupsis || You have potential. Unveil it.</title>
+	<meta charset="utf-8"> 
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Alfa+Slab+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet" href="css/global.css">
+</head>
+<body>
+	<header>
+		<div class="container_12 clearfix">
+			<h1 class="grid_4">
+				<a href="">
+					<strong>Apokalupsis</strong>
+					<span>You have potential. Unveil it</span>
+				</a>
+			</h1>
+			<div id="tag" class="grid_8">
+				<input type="text" id="search" placeholder="Search">
+			</div>
+			<nav class="grid_8 clearfix">
+				<div>
+					<a href="">Home</a>
+					<a href="">Faith</a>
+					<a href="">Health</a>
+					<a href="">Recipes</a>
+					<a href="">Resources</a>
+					<a href="">About</a>
+				</div>
+			</nav>
+		</div>
+	</header>
