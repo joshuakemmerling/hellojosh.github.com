0eb090dcf349f26202f007282b220a2009055f67
Joshua Kemmerling <joshua.kemmerling@projekt202.com>
2014-02-21 13:02:45 -0600

    demo update

--- vuejs/js/global.js
+++ vuejs/js/global.js
@@ -31,88 +31,105 @@ Vue.directive('selected', {
 
 window.onload = init;
 
+// all bugs √
+// new bug √
+// bug details √
+// users √
+// user √
+// new user √
+
 function init () {
 	Vue.component('user-select', {
 		template: $('#user_select_comp_tmpl').html()
 	});
 
-	index();
+	bug_id();
 
 	// page.start();
 }
 
 function index () {
-	$('#bugs').html($('#bugs_tmpl').html());
+	get_data('/v1/bugs', function (ddd) {
+		$('#bugs').html($('#bugs_tmpl').html());
 
-	new Vue({
-		el: '#bugs',
-		data: {
-			bugs: bugs
-		}
-	});
+		new Vue({
+			el: '#bugs',
+			data: {
+				bugs: ddd
+			}
+		});
+	}, {});
 }
 
 function bug_new () {
-	$('#bugs').html($('#bugs_new_tmpl').html());
+	get_data('/v1/users', function (ddd) {
+		$('#bugs').html($('#bugs_new_tmpl').html());
 
-	new Vue({
-		el: '#bugs',
-		data: {
-			users: users
-		},
-		methods: {
-			createBug: function () {
-				var title = $('#bug_title').val(),
-					assigned_id = parseInt($('#bug_user').val());
+		new Vue({
+			el: '#bugs',
+			data: {
+				users: ddd
+			},
+			methods: {
+				createBug: function () {
+					var title = $('#bug_title').val(),
+						assigned_id = parseInt($('#bug_user').val());
 
-				bugs.push({ id: bugs.length + 1, title: title, status: 'open', assignedto: assigned_id });
+					get_data('/v1/bug/new', function (data) {
+						index();
+					}, { title: title, status: 'open', assignedto: assigned_id });
 
-				page('/');
+					// page('/');
+				}
 			}
-		}
-	});
+		});
+	}, {});
 }
 
 function bug_id (ctx, next) {
-	$('#bugs').html($('#bugs_id_tmpl').html());
+	// var pid = parseInt(ctx.params.id),
+	var pid = 1,
+		bug = {};
 
-	var pid = parseInt(ctx.params.id),
-		bug = {},
-		user = '';
+	var callback = function (ddd) {
+		bug = ddd;
 
-	for (var i in bugs) {
-		if (bugs[i].id === pid)
-			bug = bugs[i];
-	}
+		get_data('/v1/users', callback2, {});
+	};
 
-	for (var i in users) {
-		if (users[i].id === bug.assignedto)
-			user = users[i].name;
-	}
+	var callback2 = function (ddd) {
+		$('#bugs').html($('#bugs_id_tmpl').html());
 
-	new Vue({
-		el: '#bugs',
-		data: {
-			title: bug.title,
-			status: bug.status,
-			assignedto: user,
-			users: users
-		},
-		methods: {
-			updateBug: function () {
-				for (var i in bugs) {
-					var tbb = bugs[i];
+		new Vue({
+			el: '#bugs',
+			data: {
+				title: bug[0].title,
+				status: bug[0].status,
+				assignedto: bug[0].name,
+				users: ddd
+			},
+			methods: {
+				updateBug: function () {
+					// for (var i in bugs) {
+					// 	var tbb = bugs[i];
 
-					if (tbb.id === pid) {
-						tbb.assignedto = parseInt($('#bug_user').val());
-						tbb.status = $('#bug_status').val() + '';
-					}
+					// 	if (tbb.id === pid) {
+					// 		tbb.assignedto = parseInt($('#bug_user').val());
+					// 		tbb.status = $('#bug_status').val() + '';
+					// 	}
+					// }
+
+					// page('/');
+
+					get_data('/v1/bug/update', function () {
+						index();
+					}, { id: pid, status: $('#bug_status').val() + '', assignedto: $('#bug_user').val() });
 				}
-
-				page('/');
 			}
-		}
-	});
+		});
+	};
+
+	get_data('/v1/bug', callback, { id: pid });
 }
 
 function all_user () {
@@ -151,44 +168,37 @@ function user_new () {
 }
 
 function user_details (ctx, next) {
-	var uid = parseInt(ctx.params.id),
-		fullurl = URL + '/v1/user?access_key=' + API_KEY + '&id=' + uid + '&callback=?';
+	var uid = 7,
+		users = [],
+		bugs = [];
 
 	var callback = function (data) {
-		console.log(data[0]);
+		users = data[0];
 
+		get_data('/v1/user/bugs', callback2, { id: uid });
+	};
+
+	var callback2 = function (data) {
 		$('#bugs').html($('#user_id_tmpl').html());
 
 		new Vue({
 			el: '#bugs',
 			data: {
-				bugs: _.filter(bugs, { assignedto: uid }),
-				user: data[0]
-			},
-			methods: {
-				createUser: function () {
-					var name = $('#user_name').val();
-
-					users.push({ id: users.length + 1, name: name });
-
-					page('/user');
-				}
+				user: users,
+				bugs: data
 			}
 		});
 	};
 
-	$.getJSON(fullurl, callback).error(callback);
+	get_data('/v1/user', callback, { id: uid });
 }
 
-function get_data (uri, callback) {
+function get_data (uri, callback, uri_data) {
 	var fullurl = URL + uri + '?access_key=' + API_KEY + '&callback=?';
 
-	$.getJSON(fullurl, function (data) {
-		callback(data);
-	}).error(function () {
+	$.getJSON(fullurl, uri_data, function (ddd) {
+		callback(ddd);
+	}).error(function (e) {
 		callback([]);
 	});
 }
-
-
-
